ddaf1303afb8c869e75fe6afd7bf15ac582e5fd3
hjasaiwal114 <hjasaiwal114@gmail.com>
2024-11-10 06:00:30 -0800

    feat: Placeholder in source Image

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>
         Go to About screen
@@ -18,6 +23,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
   text: {
     color: "#fff",
